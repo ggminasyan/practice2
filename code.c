97396cf764db7cc73bf5e88a7fe82f108a30adbd
ggminasyan <gminasyan@ucsd.edu>
2024-11-21 21:14:31 -0800

    Create code.c

---
+++ code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#inlclude <stdint.h>
+#include "jstr.h"
+
+String* shorter (String* str1, String* str2)
+{  
+  if (str1->length > str2->length)
+  {
+
+  }
+}
